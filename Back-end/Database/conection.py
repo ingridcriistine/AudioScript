3aa8996bced6761810c2f335204faf14d331aa39
matiaszo <tiaszuniga@gmail.com>
2025-05-05 15:38:10 -0300

    successfully connected with AWS RDS, tables created

---
+++ Back-end/Database/conection.py
@@ -0,0 +1,9 @@
+import pymysql
+
+def connect_to_aws_rds():
+    db = pymysql.connect(host='audioscript-rds.c7w666ks6bhc.sa-east-1.rds.amazonaws.com', 
+                        user='matias', 
+                        password='12345678')
+    cursor = db.cursor()
+    return cursor, db
+
