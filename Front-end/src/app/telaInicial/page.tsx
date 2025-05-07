d9ea84d72258d7894cd5fabfc5e1c68a0de276e7
milena579 <milecalegari@gmail.com>
2025-05-07 13:54:51 -0300

    terminando

--- Front-end/src/app/telaInicial/page.tsx
+++ Front-end/src/app/telaInicial/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { Menu } from "@/components/menu";
 import { useState } from "react";
 import { useRef } from "react";
 
@@ -44,77 +45,82 @@ const handleButtonClick = () => {
 
   return (
     <>
-      <div className="w-[50%] p-8 text-white">
+      <Menu/>
+      <div className="flex">
+        {/* <Submenu/> */}
+        <div className="w-[50%] p-8 text-white pt-[150px] pl-[80px]">
 
-        <button onClick={handleButtonClick}
-            className="bg-gray-800 rounded-xl p-4 w-[50%] flex items-center justify-center cursor-pointer"
-        >
-            Escolher arquivos
-        </button>
+          <button onClick={handleButtonClick}
+              className="bg-gray-800 rounded-xl p-4 w-[50%] flex items-center justify-center cursor-pointer"
+          >
+              Escolher arquivos
+          </button>
 
-        {/* Input de arquivo oculto */}
-        <input
-            type="file"
-            multiple
-            onChange={handleFileChange}
-            ref={fileInputRef}
-            className="hidden"
-        />
-
-        <ul className="mt-2">
-          {arquivos.map((arquivo) => (
-            <li key={arquivo.id} className="text-sm flex justify-between items-center bg-gray-800 p-2 rounded-xl">
-              {arquivo.name}
-              <button
-                onClick={() => handleExcluirArquivo(arquivo.id)}
-                className="text-red-500 hover:text-red-700 ml-2"
-              >
-                Excluir
-              </button>
-            </li>
-          ))}
-        </ul>
-
-        {/* Nome do arquivo transcrito */}
-        <div className="flex flex-col gap-2">
-          <label htmlFor="">Nome do arquivo transcrito</label>
+          {/* Input de arquivo oculto */}
           <input
-            type="text"
-            className="text-white border rounded-sm p-1 border-amber-50"
+              type="file"
+              multiple
+              onChange={handleFileChange}
+              ref={fileInputRef}
+              className="hidden"
           />
-        </div>
 
-        {/* Seleção de arquivo restrito */}
-        <div className="flex gap-2 mt-4">
-          <h1>Arquivo Restrito: </h1>
-          <div className="flex gap-5">
-            <label>
-              <input
-                type="radio"
-                name="restrito"
-                value="sim"
-                checked={restrito === "sim"}
-                onChange={handleRestritoChange}
-              />
-              Sim
-            </label>
+          <ul className="mt-2">
+            {arquivos.map((arquivo) => (
+              <li key={arquivo.id} className="text-sm flex justify-between items-center bg-gray-800 p-2 rounded-xl">
+                {arquivo.name}
+                <button
+                  onClick={() => handleExcluirArquivo(arquivo.id)}
+                  className="text-red-500 hover:text-red-700 ml-2"
+                >
+                  Excluir
+                </button>
+              </li>
+            ))}
+          </ul>
 
-            <label>
-              <input
-                type="radio"
-                name="restrito"
-                value="nao"
-                checked={restrito === "nao"}
-                onChange={handleRestritoChange}
-              />
-              Não
-            </label>
+          {/* Nome do arquivo transcrito */}
+          <div className="flex flex-col gap-2">
+            <label htmlFor="">Nome do arquivo transcrito</label>
+            <input
+              type="text"
+              className="text-white border rounded-sm p-1 border-amber-50"
+            />
           </div>
+
+          {/* Seleção de arquivo restrito */}
+          <div className="flex gap-2 mt-4">
+            <h1>Arquivo Restrito: </h1>
+            <div className="flex gap-5">
+              <label>
+                <input
+                  type="radio"
+                  name="restrito"
+                  value="sim"
+                  checked={restrito === "sim"}
+                  onChange={handleRestritoChange}
+                />
+                Sim
+              </label>
+
+              <label>
+                <input
+                  type="radio"
+                  name="restrito"
+                  value="nao"
+                  checked={restrito === "nao"}
+                  onChange={handleRestritoChange}
+                />
+                Não
+              </label>
+            </div>
+          </div>
+
+          <button className="bg-amber-600 p-3 rounded-xl cursor-pointer">
+            Transcrever
+          </button>
         </div>
 
-        <button className="bg-amber-600 p-3 rounded-xl cursor-pointer">
-          Transcrever
-        </button>
       </div>
     </>
   );
