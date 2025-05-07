9f0b44c82a332adc5b57dfebb8240a8f384c6793
milena579 <milecalegari@gmail.com>
2025-05-07 15:16:56 -0300

    aparentemente finalizado

--- Front-end/src/app/telaInicial/page.tsx
+++ Front-end/src/app/telaInicial/page.tsx
@@ -3,6 +3,8 @@ import { Menu } from "@/components/menu";
 import { Submenu } from "@/components/submenu";
 import { useState } from "react";
 import { useRef } from "react";
+import Lixeira from "@/assets/bin.png";
+import { ArquivoItem } from "@/components/upaloadArquivo";
 
 type Arquivo = {
   name: string;
@@ -49,79 +51,83 @@ const handleButtonClick = () => {
       <Menu/>
       <div className="flex">
         <Submenu/>
-        <div className="w-[50%] p-8 text-white pt-[150px] pl-[80px]">
+        <div className="flex w-full">
+          <div className="flex flex-col w-[50%] p-8 gap-8 text-white pt-[150px] pl-[80px]">
+            <div className="flex flex-col w-[50%] ">
+              <button onClick={handleButtonClick}
+                  className="bg-[#272727] rounded-xl p-4 flex items-center justify-center cursor-pointer hover:bg-[#333333]"
+              >
+                  Escolher arquivos
+              </button>
 
-          <button onClick={handleButtonClick}
-              className="bg-gray-800 rounded-xl p-4 w-[50%] flex items-center justify-center cursor-pointer"
-          >
-              Escolher arquivos
-          </button>
+              {/* Input de arquivo oculto */}
+              <input
+                  type="file"
+                  multiple
+                  onChange={handleFileChange}
+                  ref={fileInputRef}
+                  className="hidden"
+              />
 
-          {/* Input de arquivo oculto */}
-          <input
-              type="file"
-              multiple
-              onChange={handleFileChange}
-              ref={fileInputRef}
-              className="hidden"
-          />
+              <ul className="mt-2 gap-2 flex flex-col w-full overflow-x-auto max-h-[280px]">
+                {arquivos.map((arquivo) => (
+                  <ArquivoItem
+                    key={arquivo.id}
+                    id={arquivo.id}
+                    name={arquivo.name}
+                    onExcluir={handleExcluirArquivo}
+                  />
+                ))}
+              </ul>
 
-          <ul className="mt-2">
-            {arquivos.map((arquivo) => (
-              <li key={arquivo.id} className="text-sm flex justify-between items-center bg-gray-800 p-2 rounded-xl">
-                {arquivo.name}
-                <button
-                  onClick={() => handleExcluirArquivo(arquivo.id)}
-                  className="text-red-500 hover:text-red-700 ml-2"
-                >
-                  Excluir
-                </button>
-              </li>
-            ))}
-          </ul>
-
-          {/* Nome do arquivo transcrito */}
-          <div className="flex flex-col gap-2">
-            <label htmlFor="">Nome do arquivo transcrito</label>
-            <input
-              type="text"
-              className="text-white border rounded-sm p-1 border-amber-50"
-            />
-          </div>
-
-          {/* Seleção de arquivo restrito */}
-          <div className="flex gap-2 mt-4">
-            <h1>Arquivo Restrito: </h1>
-            <div className="flex gap-5">
-              <label>
-                <input
-                  type="radio"
-                  name="restrito"
-                  value="sim"
-                  checked={restrito === "sim"}
-                  onChange={handleRestritoChange}
-                />
-                Sim
-              </label>
-
-              <label>
-                <input
-                  type="radio"
-                  name="restrito"
-                  value="nao"
-                  checked={restrito === "nao"}
-                  onChange={handleRestritoChange}
-                />
-                Não
-              </label>
             </div>
+              
+            {/* Nome do arquivo transcrito */}
+            <div className="flex flex-col gap-2 w-full">
+              <label htmlFor="">Nome do arquivo transcrito</label>
+              <input
+                type="text"
+                className="flex text-white border rounded-sm p-1 border-amber-50 w-[50%]"
+              />
+            </div>
+
+            {/* Seleção de arquivo restrito */}
+            <div className="flex gap-2 mt-4">
+              <h1>Arquivo Restrito: </h1>
+              <div className="flex gap-5">
+                <label>
+                  <input
+                    type="radio"
+                    name="restrito"
+                    value="sim"
+                    checked={restrito === "sim"}
+                    onChange={handleRestritoChange}
+                  />
+                  Sim
+                </label>
+
+                <label>
+                  <input
+                    type="radio"
+                    name="restrito"
+                    value="nao"
+                    checked={restrito === "nao"}
+                    onChange={handleRestritoChange}
+                  />
+                  Não
+                </label>
+              </div>
+            </div>
+
+            <button className="bg-amber-600 p-3 rounded-xl cursor-pointer w-[50%]">
+              Transcrever
+            </button>
           </div>
-
-          <button className="bg-amber-600 p-3 rounded-xl cursor-pointer">
-            Transcrever
-          </button>
+              
+          <div className="w-[50%] flex pt-[150px] p-4 ">
+              <div className=" bg-white w-[80%]"></div>
+          </div>
         </div>
-
       </div>
     </>
   );
