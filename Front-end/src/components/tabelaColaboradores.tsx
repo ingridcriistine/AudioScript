dd1f170d4244232fbf5283345e7bb27ef70c437f
milena579 <milecalegari@gmail.com>
2025-05-12 15:13:13 -0300

    finalizado page colaboradores

--- Front-end/src/components/tabelaColaboradores.tsx
+++ Front-end/src/components/tabelaColaboradores.tsx
@@ -8,10 +8,10 @@ interface Linha {
 
 export const Colaborador = ({id, name, email} : Linha) => {
     return(
-        <div className="flex gap-2">
-            <span className="w-[20%] bg-gray-500 h-8">{id}</span>
-            <span className="w-[40%] bg-gray-500 h-8">{name}</span>
-            <span className="w-[40%] bg-gray-500 h-8">{email}</span>
+        <div className="flex gap-1">
+            <span className="w-[20%] bg-[#272725] h-8 rounded-sm p-2 items-center flex">{id}</span>
+            <span className="w-[40%] bg-[#272725] h-8 rounded-sm p-2 items-center flex">{name}</span>
+            <span className="w-[40%] bg-[#272725] h-8 rounded-sm p-2 items-center flex">{email}</span>
         </div>
     )
 }
