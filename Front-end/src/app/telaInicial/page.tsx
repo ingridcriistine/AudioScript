568a748026dae45986f84ad1caa24d298ae7e33d
matiaszo <tiaszuniga@gmail.com>
2025-05-23 15:56:50 -0300

    multiple files bug fixed

--- Front-end/src/app/telaInicial/page.tsx
+++ Front-end/src/app/telaInicial/page.tsx
@@ -8,6 +8,7 @@ import { ArquivoItem } from "@/components/upaloadArquivo";
 import { ROUTES } from "@/constants/routes";
 
 type Arquivo = {
+  file: File
   name: string;
   id: string;
 };
@@ -28,10 +29,10 @@ export default function TelaInicial() {
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
-      // Converte FileList para um array e atualiza o estado, gerando um ID único para cada arquivo
-      const novosArquivos = Array.from(e.target.files).map((file) => ({
+      const novosArquivos: Arquivo[] = Array.from(e.target.files).map((file) => ({
+        file,
         name: file.name,
-        id: `${file.name}-${Date.now()}`, // Criando um ID único com base no nome e timestamp
+        id: `${file.name}-${Date.now()}`
       }));
       setArquivos((prevArquivos) => [...prevArquivos, ...novosArquivos]);
     }
@@ -47,18 +48,17 @@ export default function TelaInicial() {
 
   const handleUpload = async () => {
     const formData = new FormData();
+    console.log(arquivos.length)
+    console.log("abunamalasdjpoci")
 
-    if (fileInputRef.current?.files) {
-      const files = fileInputRef.current.files;
-      for (let i = 0; i < files.length; i++) {
-        formData.append('files', files[i]); 
-      }
+    for (let i = 0; i < arquivos.length; i++){
+      formData.append('files', arquivos[i].file)
+      console.log(arquivos[i].name)
     }
+
     const customFileName = document.getElementById('customFileName') as HTMLInputElement
     formData.append('user-file-name', customFileName.value)
     formData.append('restrito', restrito);
-    console.log(customFileName.value)
-    console.log(restrito)
 
     try {
       const response = await fetch(ROUTES.upload_files, {
@@ -93,7 +93,6 @@ export default function TelaInicial() {
                   Escolher arquivos
                 </button>
 
-                {/* Input de arquivo oculto */}
                 <input
                   type="file"
                   multiple
@@ -115,7 +114,6 @@ export default function TelaInicial() {
 
               </div>
 
-              {/* Nome do arquivo transcrito */}
               <div className="flex flex-col gap-2 w-full">
                 <label htmlFor="">Nome do arquivo transcrito</label>
                 <input
